8c4b81baab30a9ecc746bbc080f0704ba6507246
Dvoynikov Kirill <illdv@mail.ru>
2018-03-02 20:52:08 +0300

    two day

--- src/scripts.js
+++ src/scripts.js
@@ -1,18 +1,57 @@
 const calc = document.querySelector('#calc');
+const displayCost = document.querySelectorAll('.costRes');
+const displayLast = document.querySelectorAll('.lastMounth');
+const displayCurrent = document.querySelectorAll('.currentMonth');
+const displayDiff = document.querySelectorAll('.diffRes');
 
 const price = {
-  sink: 1,
-  hot: 2,
-  cold: 3,
-  gas: 4,
-  el: 5,
+  sink: 4,
+  hot: 5,
+  cold: 6,
+  gas: 7,
+  el: 8,
 };
 
-const lastMonth = [0, 0, 0, 0];
-const currentMonth = [1, 2, 3, 4];
-const difference = currentMonth.map((value, index) => value - lastMonth[index]);
+function displaying(nodes, objValue) {
+  nodes.forEach((node, index) => {
+    const firstChild = node.firstChild;
+    node.insertBefore(`<span>${Object.values(objValue)[index]}</span>`, firstChild);
+  });
+}
+displaying(displayCost, price);
+
+const lastMonth = {
+  hot: 1,
+  cold: 2,
+  gas: 3,
+  el: 4,
+};
+
+displaying(displayLast, lastMonth);
+
+const currentMonth = {
+  hot: 8,
+  cold: 7,
+  gas: 6,
+  el: 5,
+};
+displaying(displayCurrent, currentMonth);
+
+function calcDifference(last, current) {
+  const objDiff = Object.values(last).reduce((acc, lastValue, index) => {
+    const currentValue = Object.values(current)[index];
+    const currentKeys = Object.keys(current)[index];
+    const diff = currentValue - lastValue;
+    acc[currentKeys] = diff;
+    return acc;
+  }, {});
+
+  return objDiff;
+}
+
+displaying(displayDiff, calcDifference(lastMonth, currentMonth));
 
 function countUp(e) {
-  console.log(e);
+  console.log('!!');
 }
 calc.addEventListener('click', countUp);
