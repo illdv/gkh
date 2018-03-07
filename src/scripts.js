4b0a5935f6a1a8566fc338718f1bf3f112249c77
Dvoynikov Kirill <illdv@mail.ru>
2018-03-07 16:39:00 +0300

    delete extra obj

--- src/scripts.js
+++ src/scripts.js
@@ -6,7 +6,7 @@ let prices = JSON.parse(localStorage.getItem('prices')) || '';
 
 let lastMonth = JSON.parse(localStorage.getItem('lastMonth')) || '';
 
-let plan = JSON.parse(localStorage.getItem('plan')) || '';
+let plan = localStorage.getItem('plan') || '';
 
 function displaying(nodes, obj) {
   nodes.forEach((node, index) => {
@@ -21,7 +21,6 @@ displaying(domElements.costs, prices);
 displaying(domElements.lasts, lastMonth);
 domElements.plan.value = plan;
 
-const objDiff = {};
 function calcDifference(last, current) {
   return getSplitObj(last).reduce((acc, lastValue, index) => {
     const currentValue = getSplitObj(current)[index];
@@ -29,10 +28,9 @@ function calcDifference(last, current) {
     const diff = currentValue - lastValue;
     acc[currentKeys] = !diff ? null : diff;
     return acc;
-  }, objDiff);
+  }, {});
 }
 
-const objTariff = {};
 function calcTariff(diff, price) {
   return getSplitObj(price).reduce((acc, priceValue, index) => {
     const diffValue = getSplitObj(diff)[index - 1];
@@ -40,7 +38,7 @@ function calcTariff(diff, price) {
     const forTariff = priceKey === 'sink' ? diff.hot * price.sink : diffValue * priceValue;
     acc[priceKey] = !forTariff ? null : +forTariff.toFixed(2);
     return acc;
-  }, objTariff);
+  }, {});
 }
 
 const calcTotal = tariff => getSplitObj(tariff).reduce((sum, value) => sum + value);
@@ -63,10 +61,12 @@ function countUp() {
 
   localStorage.setItem('prices', JSON.stringify(prices));
   localStorage.setItem('lastMonth', JSON.stringify(currentMonth));
-  localStorage.setItem('plan', JSON.stringify(plan));
+  localStorage.setItem('plan', plan);
 
-  displaying(domElements.diffs, calcDifference(lastMonth, currentMonth));
-  displaying(domElements.tariffs, calcTariff(objDiff, prices));
+  const objDiff = calcDifference(lastMonth, currentMonth);
+  const objTariff = calcTariff(objDiff, prices);
+  displaying(domElements.diffs, objDiff);
+  displaying(domElements.tariffs, objTariff);
 
   const totalValue = calcTotal(objTariff);
   domElements.total.textContent = totalValue;
